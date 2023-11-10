4a9753bab1880e93cdffd51a958e41e2c84fd1bd
learnwithancor <ancorpoint0@gmail.com>
2023-11-11 01:40:02 +0600

    commit again from shake

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
         <div className='relative bg-black'>
             <img className='h-[44rem] w-full object-cover opacity-70' src={img} alt="" />
             <div className='absolute top-0 flex justify-center items-center text-white flex-col h-full w-full px-0'>
-                <p className='text-xl'>Let's travel the world</p>
-                <p className='text-4xl md:text-5xl lg:text-6xl text-center leading-normal font-bold lg:leading-normal tracking-widest'>Explore Patagonia With <br /> <span className='stroke'>Patagonia Explore</span></p>
+                <p className='text-xl uppercase'>Let's travel the world</p>
+                <p className='text-4xl md:text-5xl lg:text-6xl text-center leading-normal font-bold lg:leading-normal tracking-widest uppercase'>Explore Patagonia With <br /> <span className='stroke'>Patagonia Explore</span></p>
             </div>
             
         </div>
