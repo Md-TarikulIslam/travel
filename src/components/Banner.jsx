a9514262c330f7f03e6dbeeec0c3504d269a0432
MdTarikulIslam <nayon1820@gmail.com>
2023-11-10 19:25:53 +0600

    10th Nov, 2023

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,13 @@
 import img from '../assets/images/banner/1.jpeg'
 
+
 const Banner = () => {
     return (
         <div className='relative'>
             <img className='h-[44rem] w-full object-cover' src={img} alt="" />
-            <div className='absolute top-0 flex justify-center items-center flex-col h-full w-full'>
+            <div className='absolute top-0 flex justify-center items-center flex-col h-full w-full px-0'>
                 <p className='text-xl'>Let's travel the world</p>
-                <p className='text-4xl md:text-5xl lg:text-6xl text-center leading-normal font-bold lg:leading-normal tracking-widest'>EXPLORE THE PATAGONIA WITH PATAGONIA TRAVEL</p>
+                <p className='text-4xl md:text-5xl lg:text-6xl text-center leading-normal font-bold lg:leading-normal tracking-widest'>Explore Patagonia With <br /> <span className='stroke'>Patagonia Explore</span></p>
             </div>
         </div>
     )
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
     Navbar,
     MobileNav,
     Typography,
     Button,
     IconButton,
-  
+
 } from "@material-tailwind/react";
 
 export default function Header() {
     const [openNav, setOpenNav] = React.useState(false);
+    const [navbarColor, setNavbarColor] = React.useState("transparent");
 
     React.useEffect(() => {
         window.addEventListener(
             "resize",
             () => window.innerWidth >= 960 && setOpenNav(false),
         );
+    }, []);
+
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            if (scrollPosition > 100) {
+                setNavbarColor("white");
+            } else {
+                setNavbarColor("transparent");
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
     const navList = (
@@ -74,73 +93,73 @@ export default function Header() {
     );
 
     return (
-        <div className="max-w-screen-3xl mx-auto w-full fixed bg-white top-0 z-10">
-            <Navbar className=" shadow-none max-w-screen-2xl  mx-auto w-full rounded-none border-none px-4 py-2 lg:px-8 lg:py-4">
-                <div className="flex items-center justify-between text-blue-gray-900 w-full">
-                    <Typography
-                        as="a"
-                        href="#"
-                        className="mr-4 cursor-pointer py-1.5 font-medium"
-                    >
-                        Patagonia
-                    </Typography>
-                    <div className="flex items-center gap-4">
-                        <div className="mr-4 hidden lg:block">{navList}</div>
-                            <Button
-                                variant="gradient"
-                                size="sm"
-                                className="hidden lg:inline-block"
-                            >
-                                <span>Booking</span>
-                            </Button>
-                        <IconButton
-                            variant="text"
-                            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-                            ripple={false}
-                            onClick={() => setOpenNav(!openNav)}
-                        >
-                            {openNav ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    className="h-6 w-6"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-6 w-6"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                </svg>
-                            )}
-                        </IconButton>
-                    </div>
-                </div>
-                <MobileNav open={openNav}>
-                    {navList}
-                      
-                        <Button fullWidth variant="gradient" size="sm" className="">
-                            <span>Booking</span>
-                        </Button>
-                </MobileNav>
-            </Navbar>
 
-        </div>
+        <Navbar className={`fixed top-0 z-50 shadow-none max-w-screen-3xl  mx-auto w-full rounded-none border-none px-4 py-2 lg:px-8 lg:py-4 transition-all duration-300 bg-${navbarColor}`}>
+            <div className=" container max-w-screen-xl mx-auto flex items-center justify-between text-blue-gray-900 w-full">
+                <Typography
+                    as="a"
+                    href="#"
+                    className="mr-4 cursor-pointer py-1.5 font-medium"
+                >
+                    Patagonia
+                </Typography>
+                <div className="flex items-center gap-4">
+                    <div className="mr-4 hidden lg:block">{navList}</div>
+                    <Button
+                        color="red"
+                        size="sm"
+                        className="hidden lg:inline-block bg-red-500 tracking-widest"
+                    >
+                        <span>Book now</span>
+                    </Button>
+                    <IconButton
+                        variant="text"
+                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                        ripple={false}
+                        onClick={() => setOpenNav(!openNav)}
+                    >
+                        {openNav ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                className="h-6 w-6"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                        )}
+                    </IconButton>
+                </div>
+            </div>
+            <MobileNav open={openNav}>
+                {navList}
+
+                <Button fullWidth color="red" size="sm" className="bg-red-500">
+                    <span>Book now</span>
+                </Button>
+            </MobileNav>
+        </Navbar>
+
+
     );
 }
