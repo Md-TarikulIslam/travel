097875ba0e66577f7be22325de801fa5b02c601f
learnwithancor <ancorpoint0@gmail.com>
2023-11-17 01:34:55 +0600

    commited

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,17 +1,21 @@
-import GalleryBanner from '../components/GalleryBanner';
-import BookNowBanner from '../components/BookNowBanner';
-import ImageGallery from '../components/ImageGallery';
+import BookNowBanner from "../components/BookNowBanner";
+import GalleryBanner from "../components/GalleryBanner";
+import ImageGallery from "../components/ImageGallery";
+import Review from "../components/Review";
 
 const GalleryPage = () => {
-    return (
-        <>
-           <GalleryBanner />
-           <div className='my-32 px-4'>
-            <ImageGallery />
-           </div>
-           <BookNowBanner />
-        </>
-    );
+  return (
+    <>
+      <GalleryBanner />
+      <div className="my-32 px-4">
+        <ImageGallery />
+      </div>
+      <BookNowBanner />
+      <div className="py-40 px-4 bg-[#f4f5f8]">
+        <Review />
+      </div>
+    </>
+  );
 };
 
 export default GalleryPage;
