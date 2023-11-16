5b42a107cddf7dc77cee6d9b95d07b3436b7ab25
learnwithancor <ancorpoint0@gmail.com>
2023-11-16 18:14:43 +0600

    commited

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import AboutBanner from '../components/AboutBanner';
+import DiscoverWorld from "../components/DiscoverWorld"
+
 
 const AboutPage = () => {
     return (
         <>
             <AboutBanner />
+            <div className="pt-32 pb-20 px-4">
+        <DiscoverWorld />
+      </div>
         </>
     );
 };
