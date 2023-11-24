a801c516d0597e468bfcc46391b4cfd1d4ab503a
learnwithancor <ancorpoint0@gmail.com>
2023-11-24 22:55:15 +0600

    complete

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -167,7 +167,7 @@ const Footer = () => {
           </div>
         </div>
       <div className="mt-6 md:mb-0 mb-6">
-        <p className="text-[#ffffffcc] text-sm text-center">©2023 <a target="blank" href="https://shakiqur-rahman-v2.netlify.app/#" className="text-blue-500">Shake's Team</a> All rights reserved.</p>
+        <p className="text-[#ffffffcc] text-sm text-center">©2023 <a target="blank" href="https://shakiqur-rahman.netlify.app/#" className="text-blue-500">Shake's Team</a> All rights reserved.</p>
       </div>
       </div>
     </footer>
