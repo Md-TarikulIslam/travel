a510314ea94164a6ab34a130c2750cde22e660e3
MdTarikulIslam <nayon1820@gmail.com>
2023-11-25 20:18:25 +0600

    25th Nov, 2023

--- src/components/TravelCountries.jsx
+++ src/components/TravelCountries.jsx
@@ -1,81 +1,136 @@
 import { Card, CardBody, Carousel } from "@material-tailwind/react";
 import img from '../assets/images/travel/1.jpg'
 import img2 from '../assets/images/travel/2.jpg'
-import {MdOutlineLocationOn } from "react-icons/md";
+import img3 from '../assets/images/travel/3.jpg'
+import { MdOutlineLocationOn } from "react-icons/md";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Pagination, Autoplay } from 'swiper/modules';
 
 export default function TravelCountries() {
     return (
-            <div className="max-width">
+        <div className="max-width">
             <p className="uppercase tracking-widest text-xs text-[#04afd1]">top destination</p>
             <p className="uppercase tracking-widest text-4xl font-bold text-[#04afd1] my-4">Travel <span className="text-[#1d355e]">countries</span></p>
-            <Carousel  className="rounded-none h-full overflow-hidden group" prevArrow={false} nextArrow={false}  loop={true} autoplay={true} autoplayDelay={4000}>
-                <div className="flex lg:flex-row flex-col items-center">
-                <div className="z-20">
-                <img className="w-[45rem] h-[570px] lg:h-[29rem] lg:group-hover:scale-95 transition-all object-contain lg:object-cover object-top" src={img} alt="" />
-                </div>
-                    <Card className="z-30 rounded-none absolute right-0 -bottom-0 lg:bottom-12  lg:right-10 lg:h-[23rem] lg:w-[30rem] ">
-                        <CardBody>
-                            <p className="uppercase tracking-widest text-xs text-[#04afd1]">travel countries</p>
-                            <p className="text-[#1d355e] font-bold text-3xl my-4">Argentina</p>
-                            <p className="text-[#848691] text-sm leading-loose">We provide you with interesting and exciting tours to different parts of the world. Orci varius natoque penatibus et magnis disney turien nascete ridiculus duru in the mus nellen.</p>
-                            <div className="grid grid-cols-3 sm:my-8 my-4 ">
-                            <div>
-                                
-                                <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> Buenos Aires</p>
-                            </div>
-                            <div>
-                                
-                                <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> El Calafate</p>
-                            </div>
-                            <div>
-                                
-                                <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> El Chalten</p>
-                            </div>
-                            </div>
-                            {/* <p className="border-b border-black"></p> */}
-                           <div className="md:absolute lg:bottom-10 bottom-5">
-                           <Link to='/destination' className="text-[#04afd1] flex items-center gap-2 mt-4 hover:underline  hover:underline-offset-4 delay-150 transition-all  ">View all places <BsArrowRight size={20}/></Link>
-                           </div>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="flex items-center flex-col lg:flex-row-reverse">
-                <div>
-                <img className="w-[45rem] h-[570px] lg:h-[29rem] lg:group-hover:scale-95 transition-all object-contain lg:object-cover object-top" src={img2} alt="" />
-                </div>
+
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                // navigation={true}
+                modules={[Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="flex lg:flex-row flex-col items-center group">
+                        <div className="z-20">
+                            <img className="w-[45rem] h-[570px] lg:h-[29rem] lg:group-hover:scale-95 transition-all object-contain lg:object-cover object-top" src={img} alt="" />
+                        </div>
+                        <Card className="z-30 rounded-none absolute right-0 -bottom-0 lg:bottom-12  lg:right-10 lg:h-[23rem] lg:w-[30rem] ">
+                            <CardBody>
+                                <p className="uppercase tracking-widest text-xs text-[#04afd1]">travel countries</p>
+                                <p className="text-[#1d355e] font-bold text-3xl my-4">Argentina</p>
+                                <p className="text-[#848691] text-sm leading-loose">We provide you with interesting and exciting tours to different parts of the world. Orci varius natoque penatibus et magnis disney turien nascete ridiculus duru in the mus nellen.</p>
+                                <div className="grid grid-cols-3 sm:my-8 my-4 ">
+                                    <div>
+
+                                        <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> Buenos Aires</p>
+                                    </div>
+                                    <div>
+
+                                        <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> El Calafate</p>
+                                    </div>
+                                    <div>
+
+                                        <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> El Chalten</p>
+                                    </div>
+                                </div>
+                                {/* <p className="border-b border-black"></p> */}
+                                <div className="md:absolute lg:bottom-10 bottom-5">
+                                    <Link to='/destination' className="text-[#04afd1] flex items-center gap-2 mt-4 hover:underline  hover:underline-offset-4 delay-150 transition-all  ">View all places <BsArrowRight size={20} /></Link>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>   <div className="flex items-center flex-col lg:flex-row-reverse group">
+                    <div>
+                        <img className="w-[45rem] h-[570px] lg:h-[29rem] lg:group-hover:scale-95 transition-all object-contain lg:object-cover object-top" src={img2} alt="" />
+                    </div>
                     <Card className="p-6 rounded-none absolute bottom-0 lg:bottom-12 lg:left-10 lg:h-[23rem] lg:w-[30rem] ">
                         <CardBody>
                             <p className="uppercase tracking-widest text-xs text-[#04afd1]">travel countries</p>
                             <p className="text-[#1d355e] font-bold text-3xl my-4">Chile</p>
                             <p className="text-[#848691] text-sm leading-loose">We provide you with interesting and exciting tours to different parts of the world. Orci varius natoque penatibus et.</p>
                             <div className="flex justify-between  sm:my-6 my-4 ">
-                            <div className="flex-1">
-                                
-                                <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> Santiago</p>
-                            </div>
-                            <div className="flex-1">
-                                
-                                <p className="flex items-start gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> Puerto Natales</p>
-                            </div>
-                            <div className="flex-1">
-                                
-                                <p className="flex items-start gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl"/> Torres Del Paine</p>
-                            </div>
+                                <div className="flex-1">
+
+                                    <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> Santiago</p>
+                                </div>
+                                <div className="flex-1">
+
+                                    <p className="flex items-start gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> Puerto Natales</p>
+                                </div>
+                                <div className="flex-1">
+
+                                    <p className="flex items-start gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" /> Torres Del Paine</p>
+                                </div>
                             </div>
                             <div className="md:absolute lg:bottom-10 bottom-5">
-                            <Link to='/destination' className="text-[#04afd1] flex items-center gap-2 mt-4 hover:underline  hover:underline-offset-4 delay-150 transition-all">View all places <BsArrowRight size={20}/></Link>
+                                <Link to='/destination' className="text-[#04afd1] flex items-center gap-2 mt-4 hover:underline  hover:underline-offset-4 delay-150 transition-all">View all places <BsArrowRight size={20} /></Link>
                             </div>
                         </CardBody>
                     </Card>
-                </div>
-           
-           
-             
-              
-              
-            </Carousel>
+                </div></SwiperSlide>
+                <SwiperSlide>  <div className="flex lg:flex-row flex-col items-center group">
+                    <div className="z-20">
+                        <img className="w-[45rem] h-[570px] lg:h-[29rem] lg:group-hover:scale-95 transition-all object-contain lg:object-cover object-top" src={img3} alt="" />
+                    </div>
+                    <Card className="z-30 rounded-none absolute right-0 -bottom-0 lg:bottom-12  lg:right-10 lg:h-[23rem] lg:w-[30rem] ">
+                        <CardBody>
+                            <p className="uppercase tracking-widest text-xs text-[#04afd1]">travel countries</p>
+                            <p className="text-[#1d355e] font-bold text-3xl my-4">Brazil</p>
+                            <p className="text-[#848691] text-sm leading-loose">We provide you with interesting and exciting tours to different parts of the world.</p>
+                            <div className="grid grid-cols-3 sm:my-8 my-4 ">
+                                <div>
+
+                                    <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" />Rio de Janeiro</p>
+                                </div>
+                                <div>
+
+                                    <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" />Foz do Iguacu</p>
+                                </div>
+                                <div>
+
+                                    <p className="flex items-center gap-1 text-sm tracking-widest font-thin"><MdOutlineLocationOn className="text-[#04afd1] text-xl" />Salvador</p>
+                                </div>
+                            </div>
+                            {/* <p className="border-b border-black"></p> */}
+                            <div className="md:absolute lg:bottom-10 bottom-5">
+                                <Link to='/destination' className="text-[#04afd1] flex items-center gap-2 mt-4 hover:underline  hover:underline-offset-4 delay-150 transition-all  ">View all places <BsArrowRight size={20} /></Link>
+                            </div>
+                        </CardBody>
+                    </Card>
+                </div></SwiperSlide>
+
+            </Swiper>
+
         </div>
     );
 }
