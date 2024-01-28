0618d1270a702ba082815717d4d5de6ab5a66d60
learnwithancor <ancorpoint0@gmail.com>
2024-01-29 01:18:55 +0600

    Itenary Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
-import img from '../src/assets/images/loader.png'
+import router from "./routes/Routes";
 
 function App() {
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-          setLoading(false);
-        }, 2000);
-      }, []);
-    return (
-        <>
-            {loading ? <div className="flex justify-center items-center h-screen w-full"><img className="animate-spin" src={img} alt="" /></div> : <RouterProvider router={router}></RouterProvider>}
-            <ToastContainer position="bottom-center" />
-        </>
-    );
+  return (
+    <>
+        <RouterProvider router={router}></RouterProvider>
+      <ToastContainer position="bottom-center" />
+    </>
+  );
 }
 
 export default App;
